cea720ecd0ff44857e9a1fc4ad74f26880907105
elliskelly99 <71240980+elliskelly99@users.noreply.github.com>
2026-09-10 14:29:45 -0400

    Update iptool.cpp

--- project/iptool.cpp
+++ project/iptool.cpp
@@ -25,19 +25,12 @@
 using namespace std;
 
 #define MAXLEN 256
-//enum functions{add, binarize, scale, colorvisual, dualthres};
-//vector<vector<image> > roi;//2d vector holding images [index][the actual image]
-//vector<vector<int> > param;//2d vector holding parameters 
+
 
 vector<vector<int> > isFunction(char *pch, int i, vector<vector<int> > param, vector<int> &func){
 	printf("is function pch:%s\n", pch);
 	printf("function i=%d\n",i);
 
-	//printf("is function *pch atoi: %d\n", atoi(pch));
-
-	// printf("FUUUU: %d\n", strncasecmp(pch,"add",MAXLEN)==0);
-	// printf("FUUUU: %d\n", strncasecmp(pch,"colorvisual",MAXLEN)==0);
-	// printf("FUUUU: %d\n", strncasecmp(pch,"binarize",MAXLEN)==0);
 	int temp = 0;
 
 	printf("about to parse\n");
@@ -54,54 +47,35 @@ vector<vector<int> > isFunction(char *pch, int i, vector<vector<int> > param, ve
 		//return 1;
 	}
 	if(strncasecmp(pch,"binarize",MAXLEN)==0){
-		//if(pch == "add"){
 		pch = strtok(NULL, " ");
-		//printf("binarize param: %s\n", pch);
-		//adjList[x].push_back();
 		temp = atoi(pch);
-		//param[i].push_back(temp);
 		param[i][0]=temp;
 
 		func[i]=1;
 
 	}
 	if(strncasecmp(pch,"scale",MAXLEN)==0){
-	//if(pch == "add"){
 		pch = strtok(NULL, " ");
-		//printf("add param: %s\n", pch);
-		//adjList[x].push_back();
 		temp = atoi(pch);
-		//param[i].push_back(temp);
 		param[i][0]=temp;
 		
 		func[i]=0;
-		//return 1;
 	}
 	if(strncasecmp(pch,"colorvisual",MAXLEN)==0){
-		//tokenize
-		//printf("int i = %d", i);
+
 		pch = strtok(NULL, " ");
-		//printf("color visual param 1: %s\n", pch);
 		param[i][0]=atoi(pch);
-		//param[i][1] = atoi(pch);
 		pch = strtok(NULL, " ");
 		param[i][1]=atoi(pch);
 
 		func[i]=3;
 
 
-		//printf("color visual param 2: %s\n", pch);
-
-		//param[i][2] = atoi(pch);
-		//return 2;	
 	}
 	if(strncasecmp(pch,"dualthres",MAXLEN)==0){
 		//tokenize
-		//printf("int i = %d", i);
 		pch = strtok(NULL, " ");
-		//printf("color visual param 1: %s\n", pch);
 		param[i][0]=atoi(pch);
-		//param[i][1] = atoi(pch);
 		pch = strtok(NULL, " ");
 		param[i][1]=atoi(pch);
 
@@ -110,11 +84,6 @@ vector<vector<int> > isFunction(char *pch, int i, vector<vector<int> > param, ve
 
 		func[i]=4;
 
-
-		//printf("color visual param 2: %s\n", pch);
-
-		//param[i][2] = atoi(pch);
-		//return 2;	
 	}
 	if(strncasecmp(pch,"histostretch",MAXLEN)==0){
 
@@ -187,33 +156,11 @@ void utility::createRoi(int x, int y, int sx, int sy ,vector <image> &roi,image 
 	int iMax = x + sx;
 	int jMax = y + sy;
 
-	//printf("in function\n");
-	//tempRoi.resize(src.getNumberOfRows(), src.getNumberOfColumns());
-	//tempRoi.resize(iMax, jMax);
+
 	tempRoi.resize(sx, sy);
-	// for (int i=x; i<=sx; i++){
-	// 	for(int j=y; j <=sy; j++){
-	// 		tempRoi.setPixel(i,j, checkValue(src.getPixel(i,j)));
-	// 	}
-	// }
 
-		// //for (int i=0; i<src.getNumberOfRows(); i++)
-		// //for (int i=x; i<(x+sx); i++)
-		// for (int i=x; i<(x+sx); i++)
-		// //for (int j=0; j<src.getNumberOfColumns(); j++)
-		// for (int j=y; j<(y+sy); j++)
-		// //for (int j=y; j<sy; j++)
-		// {
-		// 	tempRoi.setPixel(i,j,checkValue(src.getPixel(i,j)));
-		// }
-
-		//for (int i=x; i<(x+sx); i++)
 		for(int i = 0; i < (x+sx); i++){
-		//for(int i = x; i < (x+sx); i++){
-		//for (int j=0; j<src.getNumberOfColumns(); j++)
 		for (int j=0; j<(y+sy); j++)
-		//for (int j=y; j< sy; j++)
-		//for (int j=y; j<sy; j++)
 		{
 			if (isColor.find(".ppm") == string::npos) {
 				tempRoi.setPixel(i-x,j-y,checkValue(src.getPixel(i,j)));
@@ -227,10 +174,8 @@ void utility::createRoi(int x, int y, int sx, int sy ,vector <image> &roi,image 
 		}
 		}
 	
-	//roi.push_back(tempRoi);
 
 	roi[i] = tempRoi;
-	//tempRoi.save("test.ppm");
 	
 }
 
@@ -246,9 +191,7 @@ int main (int argc, char** argv)
 	char *pch;
 	char t[MAXLEN], v1[MAXLEN], v2[MAXLEN];
 	char numRoi[MAXLEN];
-	//char x[3][MAXLEN], y[3][MAXLEN], sx[3][MAXLEN], sy[3][MAXLEN];
 
-	//char *funcName;
 	char funcName[MAXLEN];
 	
 	string isPPM; 
@@ -266,9 +209,6 @@ int main (int argc, char** argv)
 	//white reads line by line
 	while(fgets(str,MAXLEN,fp) != NULL) {
 		vector<vector<int> > param;
-		//vector<vector<image> > roi;
-		//vector<image> roi;
-		//vector<image> roi(3,src);
 	
 		pch = strtok(str, " ");
 		src.read(pch);
@@ -297,10 +237,8 @@ int main (int argc, char** argv)
 		strcpy(numRoi, pch);
 
 		//testing if parsing num of ROIs worked
-		//printf("num of rois: %d\n",  atoi(numRoi));
 
 		int numDim = atoi(numRoi);
-		//char x[numDim][MAXLEN], y[numDim][MAXLEN], sx[numDim][MAXLEN], sy[numDim][MAXLEN];
 		int x[numDim], y[numDim], sx[numDim], sy[numDim];
 		
 		//will store the function name for each ROI
@@ -309,25 +247,15 @@ int main (int argc, char** argv)
 		
 
 		//token the rest of the string in a for loop with #roi as iterations 
-		//for(int i = 1; i <= 1; i++){
 		for(int i = 0; i < atoi(numRoi); i++){
 			
 			//creating matrix 
 			if(i==0){
 				//upto 4 paramters for each function so setting it 4 and 0
 			 	param.resize(atoi(numRoi), std::vector<int>(4,0));
-				//roi.resize(atoi(numRoi), std::vector<int>(5,0));
-				//roi.resize(atoi(numRoi), 1);//matrix of images
+
 			}
 
-			//printf("--------matrix---------------\n");
-
-			// for ( std::size_t i = 0; i < param.size(); i++) {
-			// 	for (std::size_t j = 0; j < param[i].size(); j++){
-            // 		std::cout << param[i][j] << " ";
-       		// 	 }	
-        	// 	std::cout << std::endl;
-  		 	//  }
 
 			//loading x			
 			pch = strtok(NULL, " ");
@@ -349,9 +277,7 @@ int main (int argc, char** argv)
 			//strcpy(sx[i], pch);
 			sy[i] = atoi(pch);
 
-			//printf("sx: %d i:%d\n",  atoi(sx[1]),i);
 
-			//printf("sx: %d i:%d\n",  sx[i],i);
 
 
 
@@ -360,24 +286,18 @@ int main (int argc, char** argv)
 			sx[i] = atoi(pch);
 
 			//strcpy(sy[i], pch);
-			//printf("sy: %d i:%d\n",  atoi(sy[1]),i);
-			//printf("sy: %d i:%d\n",  sy[i],i);
-
 
 
 			//create roi image
-			//utility::createRoi(atoi(x[i]),atoi(y[i]), atoi(sx[i]),atoi(sy[i]), roi, src, i);
 			utility::createRoi(x[i],y[i], sx[i],sy[i], roi, src, i, isPPM);
 			
 			//if next token is function then check how many parameters needed then tokenize those parameters 
 			pch = strtok(NULL, " ");
 			strcpy(funcName, pch);
-			///printf("Function name:%s\n",funcName);
 			param = isFunction(funcName, i, param, func);
 
 			
 
-			//printf("------iteration: %d\n", i);
 		}
 		printf("finsihing loop and parsing\n");
 
@@ -388,16 +308,11 @@ int main (int argc, char** argv)
 	utility::copy(src,tgt,0);
 
 	for(int i = atoi(numRoi); i >= 1; i--){
-		// int iMax = atoi(x[i-1]) + atoi(sx[i-1]);
-		// int jMax = atoi(y[i-1]) + atoi(sy[i-1]);
 
 		int iMax = x[i-1] + sx[i-1];
 		int jMax = y[i-1] +  sy[i-1];
 		printf("before going into function %d\n",func[i-1]==0);
-		//printf("i=%d \n",i);
-		//printf("imax= %d\n",iMax);
-		//for(int i = 0; i < atoi(numRoi); i++){
-		//	pch = strtok(NULL, " ");
+
 		if(func[i-1]==0){
 				utility::addGrey(roi[i-1],tempImg,param[i-1][0]);
 
@@ -446,19 +361,6 @@ int main (int argc, char** argv)
 		
 		
 		//overlapping ROI images together 
-		// for (int h=0; h<src.getNumberOfRows(); h++){
-		// 	for (int n=0; n<src.getNumberOfColumns(); n++){
-		// 		if(h >= x[i-1] && h  <= iMax){
-		// 			if(n >= y[i-1] && n <= jMax){
-		// 				//printf("in first if\n");
-		// 				tgt.setPixel(h,n,utility::checkValue(tempImg.getPixel(h-x[i-1],n- x[i-1]))); 
-				
-		// 			}
-		// 		}
-		// 	}
-		// }
-
-
 		string temp; 
 		temp = outfile;
 		
@@ -505,28 +407,12 @@ int main (int argc, char** argv)
 		//printf("fin overlap\n");
 
 	}//end for 
-	//printf("fin for loop\n");
-	//pch = strtok(NULL, " ");
-	//printf("before pch \n");
-	//printf("pch= %s", str);
-	
-	
-		// printf("--------ending matrix---------------\n");
 
-		// for ( std::size_t i = 0; i < param.size(); i++) {
-        // 	for (std::size_t j = 0; j < param[i].size(); j++) {
-        //     	std::cout << param[i][j] << " ";
-       	// 	 }
-       	// 	 std::cout << std::endl;
-   	 	// }//end for 
 
 		tgt.save(outfile);
 	}
 
-	//printf("fin while loop\n");
 
-	//tgt.save(outfile);
-	//fclose(fp);
 	return 0;
 }
 
